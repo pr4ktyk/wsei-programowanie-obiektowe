e75f717f77492f6f9411b76c17daf101a0668e08
Łukasz Wąś <ljwasiu@gmail.com>
2022-03-09 18:29:31 +0100

    lab2

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -9,8 +9,54 @@ namespace lab1
             //Money.Of(-12, Currency.PLN) == null ? Money.Of(0, Currency.PLN) : Money.Of(-12, Currency.PLN);
             Money money = Money.Of(12, Currency.PLN) ?? Money.Of(0, Currency.PLN);
 
-            Money result = money * 0.22m;
-            Console.WriteLine(result.Value);
+            Money a = money * 2m;
+            Money b = money * 10m;
+
+            Console.WriteLine($"money * 2: {a.Value};\nmoney * 10: {b.Value};");
+            Console.WriteLine();
+            Money c = money + 1m;
+            Money d = 7m + money;
+            Console.WriteLine($"money + 1: {c.Value};\n7 + money: {d.Value};");
+            Console.WriteLine();
+            Console.WriteLine($"c < d: {c < d};\nd > c: {d > c};");
+
+            int x = 10;
+            long y = 10L;
+
+            y = x;
+            x = (int)y;
+
+            decimal price = money;
+            double cost = (double)money;
+
+            float z = (float)money;
+
+            Console.WriteLine($"x == y: {x == y};");
+            Console.WriteLine();
+            Console.WriteLine($"money.Equals(12, PLN) -> {money.Equals(Money.Of(12, Currency.PLN))}");
+            Console.WriteLine($"money.Equals(13, PLN) -> {money.Equals(Money.Of(13, Currency.PLN))}");
+            Console.WriteLine();
+            Console.Write("money (wyswietla 12, bo jest niejawne rzutowanie) -> ");
+            Console.WriteLine(money);
+            Console.WriteLine($"money.ToString() -> {money}");
+
+            Person p1 = Person.OfName("Jan");
+            Console.WriteLine($"person.ToString() -> {p1}");
+
+            Money[] prices =
+            {
+                Money.Of(10, Currency.PLN),
+                Money.Of(5, Currency.EUR),
+                Money.Of(50, Currency.USD),
+                Money.Of(20, Currency.PLN)
+            };
+
+            Array.Sort(prices);
+
+            foreach(var p in prices)
+            {
+                Console.WriteLine($"{p.Value} {p.Currency};");
+            }
         }
     }
 
@@ -52,6 +98,11 @@ namespace lab1
                 }
             }
         }
+
+        public override string ToString()
+        {
+            return $"Name: {firstName};";
+        }
     }
     public enum Currency
     {
@@ -59,8 +110,7 @@ namespace lab1
         USD = 2,
         EUR = 3
     }
-
-    public class Money
+    public class Money : IEquatable<Money>, IComparable<Money>
     {
         private readonly decimal _value;
         private readonly Currency _currency;
@@ -82,6 +132,50 @@ namespace lab1
             return Money.Of(money._value * factor, money.Currency);
         }
 
+        public static Money operator *(decimal factor, Money money)
+        {
+            return Money.Of(money._value * factor, money.Currency);
+        }
+
+        public static Money operator +(Money val1, Money val2)
+        {
+            IsSameCurrencies(val1, val2);
+
+            return Money.Of(val1.Value + val2.Value, val1.Currency);
+        }
+
+        public static bool operator <(Money val1, Money val2)
+        {
+            IsSameCurrencies(val1, val2);
+
+            return val1.Value < val2.Value;
+        }
+
+        public static bool operator >(Money val1, Money val2)
+        {
+            IsSameCurrencies(val1, val2);
+
+            return val1.Value > val2.Value;
+        }
+
+        private static void IsSameCurrencies(Money val1, Money val2)
+        {
+            if (val1.Currency != val2.Currency)
+            {
+                throw new Exception();
+            }
+        }
+
+        public static Money operator +(Money money, decimal factor)
+        {
+            return Money.Of(money._value + factor, money.Currency);
+        }
+
+        public static Money operator +(decimal factor, Money money)
+        {
+            return Money.Of(money._value + factor, money.Currency);
+        }
+
         public Currency Currency
         {
             get
@@ -98,6 +192,49 @@ namespace lab1
         public static Money? OfWithException(decimal value, Currency currency)
         {
             return value < 0 ? throw new ArgumentOutOfRangeException("Wartość musi być większa niż 0.") : new Money(value, currency);
+        }
+
+        public static implicit operator decimal(Money money)
+        {
+            return money.Value;
+        }
+        public static explicit operator double(Money money)
+        {
+            return (double)money.Value;
+        }
+
+        public override string ToString()
+        {
+            return $"Value: {_value}, Currency: {_currency};";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Money);
+        }
+
+        public bool Equals(Money other)
+        {
+            return other != null &&
+                   _value == other._value &&
+                   _currency == other._currency;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_value, _currency);
+        }
+
+        public int CompareTo(Money other)
+        {
+            int result = _currency.CompareTo(other._currency);
+
+            if(result == 0)
+            {
+                result = _value.CompareTo(other._value);
+            }
+
+            return result;
         }
     }
 }
