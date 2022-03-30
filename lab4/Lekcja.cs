b6fd2bb2208ec688d51c9614cce12017c5c926a0
lukasz.was <ljwasiu@gmail.com>
2022-03-30 16:57:09 +0200

    +lab4

---
+++ lab4/Lekcja.cs
@@ -0,0 +1,121 @@
+﻿/*using System;
+
+namespace lab4
+{
+    class Lekcja
+    {
+        enum Degree
+        {
+            BardzoDobry = 50,
+            DobryPlus = 45,
+            Dobry = 40,
+            DostatecznyPlus= 35,
+            Dostateczny = 30,
+            Niedostateczny = 20
+        }
+
+        record Student(string Name, int Points, char Group);
+
+        static void Main(string[] args)
+        {
+            Degree ocena = Degree.Dostateczny;
+            Console.WriteLine(ocena);
+            Console.WriteLine((int)ocena);
+            string[] names = Enum.GetNames<Degree>();
+            Console.WriteLine(string.Join(", ", names));
+            Degree[] degrees = Enum.GetValues<Degree>();
+            foreach (var item in degrees)
+            {
+                Console.WriteLine($"{item} ({(int)item})");
+            }
+            Console.WriteLine();
+            Console.Write("Wpisz ocenę: ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                Degree degree = Enum.Parse<Degree>(input);
+                Console.WriteLine($"{degree} ({(int)degree})");
+            } catch(ArgumentException ex)
+            {
+                Console.WriteLine($"Nie istnieje ocena \"{input}\"!");
+            }
+
+            string usDegree;
+            switch (ocena)
+            {
+                case Degree.BardzoDobry:
+                    usDegree = "A";
+                    break;
+                case Degree.DobryPlus:
+                    usDegree = "B";
+                    break;
+            }
+
+            usDegree = ocena switch
+            {
+                Degree.BardzoDobry => "A",
+                Degree.DobryPlus => "B",
+                Degree.Dobry => "C",
+                Degree.DostatecznyPlus => "D",
+                Degree.Dostateczny => "E",
+                _ => "F"
+            };
+
+            Console.WriteLine($"US Degree : {usDegree}");
+
+            int points = 45;
+
+            Degree result;
+            if (points >= 50 && points < 60)
+            {
+                result = Degree.Dostateczny;
+            }
+
+            result = points switch
+            {
+                >= 50 and < 60 => Degree.Dostateczny,
+                >= 60 and < 70 => Degree.DostatecznyPlus,
+                >= 70 and < 80 => Degree.Dobry,
+                >= 80 and < 90 => Degree.DobryPlus,
+                >= 90 and < 100 => Degree.BardzoDobry,
+                _ => Degree.Niedostateczny
+            };
+
+            Student[] students =
+            {
+                new Student(Name: "Karol", Points: 80, Group: 'E'),
+                new Student(Name: "Paweł", Points: 120, Group: 'E'),
+                new Student(Name: "Ewa", Points: 70, Group: 'B'),
+                new Student(Name: "Adam", Points: 60, Group: 'B'),
+                new Student(Name: "Daniel", Points: 50, Group: 'E')
+            };
+
+            Console.WriteLine(students[1] == new Student("Paweł", 80, 'E'));
+
+            foreach(Student st in students)
+            {
+                Console.WriteLine(st);
+            }
+
+            (string, bool)[] results = new (string, bool)[students.Length];
+            for (int i = 0; i < students.Length; i++)
+            {
+                Student st = students[i];
+                results[i] = (st.Name,
+                    st switch
+                    {
+                        { Points: >= 100, Group: 'E' } => true,
+                        { Points: >= 50, Group: 'B' } => true,
+                        _ => false
+                    });
+            }
+
+            foreach (var item in results)
+            {
+                Console.WriteLine($"{item.Item1} {(item.Item2 == true ? "zdał egzamin." : "nie zdał egzaminu.")}");
+            }
+        }
+    }
+}
+*/
---
+++ lab4/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.Drawing;
+
+class App
+{
+    public static void Main(string[] args)
+    {
+        (int, int) point1 = (1, 1);
+        Direction4 dir = Direction4.UP;
+        var point2 = Exercise1.NextPoint(dir, point1, (4, 4)); // == (2, 3)
+        Console.WriteLine($"({point2.Item1}, {point2.Item2})");
+    }
+}
+
+enum Direction8
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    UP_LEFT,
+    DOWN_LEFT,
+    UP_RIGHT,
+    DOWN_RIGHT
+}
+
+enum Direction4
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
+
+//Cwiczenie 1
+//Zdefiniuj metodę NextPoint, która powinna zwracać krotkę ze współrzędnymi piksela przesuniętego jednostkowo w danym kierunku względem piksela point.
+//Krotka screenSize zawiera rozmiary ekranu (width, height)
+//Przyjmij, że początek układu współrzednych (0,0) jest w lewym górnym rogu ekranu, a prawy dolny ma współrzęne (witdh, height) 
+//Pzzykład
+//dla danych wejściowych 
+//(int, int) point1 = (2, 4);
+//Direction4 dir = Direction4.UP;
+//var point2 = NextPoint(dir, point1);
+//w point2 powinny być wartości (2, 3);
+//Jeśli nowe położenie jest poza ekranem to metoda powinna zwrócić krotkę z point
+class Exercise1
+{
+    public static (int, int) NextPoint(Direction4 direction, (int, int) point, (int, int) screenSize)
+    {
+        (int, int) newPoint = new (point.Item1, point.Item2);
+
+        newPoint = direction switch
+        {
+            Direction4.UP => (newPoint.Item1, newPoint.Item2-1),
+            Direction4.DOWN => (newPoint.Item1, newPoint.Item2+1),
+            Direction4.LEFT => (newPoint.Item1-1, newPoint.Item2),
+            Direction4.RIGHT => (newPoint.Item1+1, newPoint.Item2),
+            _ => (point.Item1, point.Item2)
+        };
+
+        if (newPoint.Item1 > screenSize.Item1 || newPoint.Item1 < 0 || newPoint.Item2 > screenSize.Item2 || newPoint.Item2 < 0) return point;
+        else return newPoint;
+    }
+}
+//Cwiczenie 2
+//Zdefiniuj metodę DirectionTo, która zwraca kierunek do piksela o wartości value z punktu point. W tablicy screen są wartości
+//pikseli. Początek układu współrzędnych (0,0) to lewy górny róg, więc punkt o współrzęnych (1,1) jest powyżej punktu (1,2) 
+//Przykład
+// Dla danych weejsciowych
+// static int[,] screen =
+// {
+//    {1, 0, 0},
+//    {0, 0, 0},
+//    {0, 0, 0}
+// };
+// (int, int) point = (1, 1);
+// po wywołaniu - Direction8 direction = DirectionTo(screen, point, 1);
+// w direction powinna znaleźć się stała UP_LEFT
+class Exercise2
+{
+    static int[,] screen =
+    {
+        {1, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
+
+    private static (int, int) point = (1, 1);
+
+    private Direction8 direction = DirectionTo(screen, point, 1);
+
+    public static Direction8 DirectionTo(int[,] screen, (int, int) point, int value)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+//Cwiczenie 3
+//Zdefiniuj metodę obliczającą liczbę najczęściej występujących aut w tablicy cars
+//Przykład
+//dla danych wejściowych
+// Car[] _cars = new Car[]
+// {
+//     new Car(),
+//     new Car(Model: "Fiat", true),
+//     new Car(),
+//     new Car(Power: 100),
+//     new Car(Model: "Fiat", true),
+//     new Car(Power: 125),
+//     new Car()
+// };
+//wywołanie CarCounter(Car[] cars) powinno zwrócić 3
+record Car(string Model = "Audi", bool HasPlateNumber = false, int Power = 100);
+
+class Exercise3
+{
+    public static int CarCounter(Car[] cars)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+record Student(string LastName, string FirstName, char Group, string StudentId = "");
+//Cwiczenie 4
+//Zdefiniuj metodę AssignStudentId, która każdemu studentowi nadaje pole StudentId wg wzoru znak_grupy-trzycyfrowy-numer.
+//Przykład
+//dla danych wejściowych
+//Student[] students = {
+//  new Student("Kowal","Adam", 'A'),
+//  new Student("Nowak","Ewa", 'A')
+//};
+//po wywołaniu metody AssignStudentId(students);
+//w tablicy students otrzymamy:
+// Kowal Adam 'A' - 'A001'
+// Nowal Ewa 'A'  - 'A002'
+//Należy przyjąc, że są tylko trzy możliwe grupy: A, B i C
+class Exercise4
+{
+    public static void AssignStudentId(Student[] students)
+    {
+
+    }
+}
