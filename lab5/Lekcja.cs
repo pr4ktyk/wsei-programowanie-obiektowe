b758c74d046ec1a6ca60e21234fb1ccb6de5399b
Łukasz Wąś <ljwasiu@gmail.com>
2022-04-20 18:26:54 +0200

    +lab7

---
+++ lab5/Lekcja.cs
@@ -0,0 +1,147 @@
+﻿/*using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace lab5
+{
+    class Lekcja
+    {
+        static void Main(string[] args)
+        {
+            Sandwitch sandwitch = new Sandwitch()
+            {
+                Bread = new Ingredient { Calories = 100, Name = "Bułka krakowska" },
+                Butter = new Ingredient { Calories = 50, Name = "Delma" },
+                Ham = new Ingredient { Calories = 400, Name = "Z kotła"},
+                Salad = new Ingredient { Calories = 10, Name = "Lodowa"}
+            };
+
+            IEnumerator<Ingredient> enumerator = sandwitch.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+            foreach (var item in sandwitch)
+            {
+                Console.WriteLine(item);
+            }
+
+            Parking parking = new Parking();
+            foreach (var item in parking)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(string.Join(", ", parking));
+            Console.WriteLine(string.Join(", ", sandwitch));
+
+            Console.WriteLine(parking['B']);
+            parking['A'] = "KT2222";
+            Console.WriteLine(string.Join(", ", parking));
+        }
+    }
+
+    class Sandwitch: IEnumerable<Ingredient>
+    {
+        public Ingredient Bread { get; init; }
+        public Ingredient Butter { get; init; }
+        public Ingredient Salad { get; init; }
+        public Ingredient Ham { get; init; }
+
+        public IEnumerator<Ingredient> GetEnumerator()
+        {
+            yield return Bread;
+            yield return Butter;
+            yield return Salad;
+            yield return Ham;
+            //return new SandwitchEnumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    record Ingredient
+    {
+        public double Calories { get; init; }
+        public string Name { get; init; }
+    }
+
+    class SandwitchEnumerator : IEnumerator<Ingredient>
+    {
+        private Sandwitch _sandwitch;
+        int counter = -1;
+
+        public SandwitchEnumerator(Sandwitch sandwitch)
+        {
+            _sandwitch = sandwitch;
+        }
+
+        public Ingredient Current
+        {
+            get
+            {
+                return counter switch
+                {
+                    0 => _sandwitch.Bread,
+                    1 => _sandwitch.Butter,
+                    2 => _sandwitch.Ham,
+                    3 => _sandwitch.Salad,
+                    _ => null
+                };
+            }
+        }
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            
+        }
+
+        public bool MoveNext()
+        {
+            return ++counter < 3;
+        }
+
+        public void Reset()
+        {
+            
+        }
+    }
+
+    class Parking: IEnumerable<string>
+    {
+        private String[] _arr = { null, "GL678", null, null, "TK32434", "AFB4234", null };
+        public string this[char slot]
+        {
+            get
+            {
+                return _arr[slot - 'A'];
+            }
+
+            set
+            {
+                _arr[slot - 'A'] = value;
+            }
+
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            foreach(string car in _arr)
+            {
+                if (car != null) yield return car;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+
+}
+*/
---
+++ lab7/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace lab7
+{
+
+    delegate double Operator(double a, double b);
+
+    class Program
+    {
+
+        public static double Addition(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public static void PrintIntArray(int[] arr, Func<int, string> formatter)
+        {
+            foreach (var item in arr)
+            {
+                Console.WriteLine(formatter.Invoke(item));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Operator operation = Addition;
+            double result = operation.Invoke(3, 6);
+            Console.WriteLine($"{result}");
+
+            operation = Multiply;
+            result = operation.Invoke(2, 4);
+            Console.WriteLine($"{result}");
+
+            Func<double, double, double> op = Multiply;
+            result = op.Invoke(3, 5);
+            Console.WriteLine($"{result}");
+
+            // Zwraca wartosc podanego typu (tutaj string)
+            Func<int, string> Formatter = delegate (int number)
+            {
+                return String.Format("0x{0:X}", number);
+            };
+
+            Func<int, string> DecFormat = delegate (int number)
+            {
+                return String.Format("{0}", number);
+            };
+
+            Console.WriteLine(Formatter.Invoke(18));
+            PrintIntArray(new int[]{10, 18, 155}, Formatter);
+            PrintIntArray(new int[]{10, 18, 155}, DecFormat);
+
+            // zwraca zawsze bool
+            Predicate<string> OnlyThreeChars = delegate(string s)
+            {
+                return s.Length == 3;
+            };
+
+            Func<int, int, int, bool> InRange = delegate (int val, int min, int max)
+            {
+                return (val > min && val < max);
+            };
+
+            // Nie zwraca nic, wykonuje polecenie
+            Action<string> Print = delegate (string s)
+            {
+                Console.WriteLine(s);
+            };
+
+            // zwraca wynik dodawania, nazwy argumentów w nawiasie, po => działanie
+            Operator AddLambda = (a, b) => a + b;
+
+            // zwraca int, nie ma argumentu (trzeba wstawić pusty nawias)
+            Func<int> Lambda = () => 5;
+
+            Action<string> PrintLambda = s => Console.WriteLine(s);
+
+            // drugi argument PrintIntArray przekazany jako lambda
+            PrintIntArray(new int[] { 3, 15, 255 }, n => string.Format("0x{0:X}", n));
+        }
+    }
+}
